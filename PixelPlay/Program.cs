87955f7f9a65aa5c0cac2c45e3184ee64d3ef19a
Jaible <jaiblemd@gmail.com>
2023-12-04 15:06:50 -0600

    Agregar archivos de proyecto.

---
+++ PixelPlay/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using PixelPlay.Models;
+
+namespace PixelPlay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<PixelPlayContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PixelPlay")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
